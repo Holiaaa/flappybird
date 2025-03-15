d59094f16b0abe4ef20322ceecd082d989ce181c
Téo J. <105713570+Holiaaa@users.noreply.github.com>
2025-03-15 20:07:51 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -217,6 +217,7 @@ int main(int argc, char *argv[]) {
         window.draw(score);
         window.display();
 
+        // since SFML doesn't support delay by default like SDL with SDL_Delay i use the sleep function with a some miniseconds to do that.
         sf::sleep(sf::milliseconds(10));
     }
 
